85d448040d36a2d7e2a49d6d7a73ae1f4b7f3c0c
Tudor Marculescu <tudor.marculescu@nxp.com>
2025-03-12 21:31:28 +0200

    Add LUT8bit and LUT16bit solution

--- invertBits/main.cpp
+++ invertBits/main.cpp
@@ -7,7 +7,9 @@ using namespace std::chrono;
 
 #define ONE_MILLION (100000000)
 
-// Done in 3950 ms
+// Done in 227 ms LUT 8 bit
+// Done in 149 ms LUT 16 bit
+// Done in
 // i7 12800H @ 2.4 Ghz
 
 void naiveInvertBits(uint32_t* pData, uint32_t dataSize) {
@@ -136,6 +138,85 @@ void optimizedInvertBits(uint32_t* pData, uint32_t dataSize) {
     }
 }
 
+/*
+ * No calculation, just LUT accesses
+ */
+uint32_t lut8Bit[4][256];
+uint32_t lut16Bit[2][65536];
+
+void computeLut8Bit() {
+    // Initialize lookup tables
+    for ( int pos = 0; pos < 4; pos++) {
+        const int global_pos = pos * 8;
+
+        for (int val = 0; val < 256; val++) { // Iterate all possible values of a byte 2^8 = 256
+            uint32_t result = 0;
+
+            // Even bits
+            for (int bit = 0; bit < 8; bit = bit + 2) { // for each bit in byte
+                // result is initialized with 0s, so no need to account for 0 bits
+                if (val & (1 << bit)) { // if bit is 1, determine if even or odd and place accordingly
+                    const int global_bit = global_pos + bit;
+                    result |= 1U << (31 - (global_bit >> 1)); // global_bit / 2
+                    // resut += 1 << (31 - (i / 2))
+                }
+            }
+            // Odd bits
+            for (int bit = 1; bit < 8; bit = bit + 2) { // for each bit in byte
+                // result is initialized with 0s, so no need to account for 0 bits
+                if (val & (1 << bit)) { // if bit is 1, determine if even or odd and place accordingly
+                    const int global_bit = global_pos + bit;
+                    result |= 1U << (15 - (global_bit >> 1)); // global_bit / 2
+                }
+            }
+            lut8Bit[pos][val] = result;
+        }
+    }
+
+}
+
+void computeLut16Bit() {
+    // Initialize lookup tables
+    for ( int pos = 0; pos < 2; pos++) {
+        const int global_pos = pos * 16;
+
+        for (int val = 0; val < 65536; val++) { // Iterate all possible values of a byte 2^8 = 256
+            uint32_t result = 0;
+
+            // Even bits
+            for (int bit = 0; bit < 16; bit = bit + 2) { // for each bit in byte
+                // result is initialized with 0s, so no need to account for 0 bits
+                if (val & (1 << bit)) { // if bit is 1, determine if even or odd and place accordingly
+                    const int global_bit = global_pos + bit;
+                    result |= 1U << (31 - (global_bit >> 1)); // global_bit / 2
+                    // resut += 1 << (31 - (i / 2))
+                }
+            }
+            // Odd bits
+            for (int bit = 1; bit < 16; bit = bit + 2) { // for each bit in byte
+                // result is initialized with 0s, so no need to account for 0 bits
+                if (val & (1 << bit)) { // if bit is 1, determine if even or odd and place accordingly
+                    const int global_bit = global_pos + bit;
+                    result |= 1U << (15 - (global_bit >> 1)); // global_bit / 2
+                }
+            }
+            lut16Bit[pos][val] = result;
+        }
+    }
+
+}
+
+void lut8BitInvertBits(uint32_t* pData, uint32_t dataSize) {
+    for (uint32_t i = 0; i < dataSize; i++) {
+        pData[i] = (lut8Bit[0][pData[i] & 0xFF] | lut8Bit[1][(pData[i]>>8) & 0xFF] | lut8Bit[2][(pData[i]>>16) & 0xFF] | lut8Bit[3][pData[i]>>24]);
+    }
+}
+
+void lut16BitInvertBits(uint32_t* pData, uint32_t dataSize) {
+    for (uint32_t i = 0; i < dataSize; i++) {
+        pData[i] = (lut16Bit[0][pData[i] & 0xFFFF] | lut16Bit[1][(pData[i]>>16)]);
+    }
+}
 
 int main() {
 
@@ -148,13 +229,19 @@ int main() {
         *(pData + i) = rand();
     }
 
+    *pData = 0xAAAAAAAA;
     uint32_t refNumber = *pData;
+
+    computeLut8Bit();
+    computeLut16Bit();
 
     auto start = std::chrono::high_resolution_clock::now();
 
     //naiveInvertBits(pData, ONE_MILLION);
-    optimizedInvertBits(pData, ONE_MILLION);
+    //optimizedInvertBits(pData, ONE_MILLION);
 
+    // lut8BitInvertBits(pData, ONE_MILLION);
+    lut16BitInvertBits(pData, ONE_MILLION);
 
     auto stop = std::chrono::high_resolution_clock::now();
     
